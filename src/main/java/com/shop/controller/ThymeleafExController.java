355b860fb95a2119564a02e7ad07fc8ef6ef1c7a
gusrl6394 <gusrl6394@naver.com>
2022-01-22 00:51:39 +0900

    bootstrap 적용

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/thymeleaf")
 public class ThymeleafExController {
     @GetMapping(value = "/")
     public String index(Model model){
-        return "/index";
-    }
-
-    @GetMapping(value = "/content")
-    public String content(Model model){
         return "/content";
     }
 }
