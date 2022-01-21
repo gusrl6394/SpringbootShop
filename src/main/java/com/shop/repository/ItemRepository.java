ab19c90f0534eedcb1501bf0a1e7024547e6494d
gusrl6394 <gusrl6394@naver.com>
2022-01-21 16:33:26 +0900

    init

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,20 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+// extends JpaRepository<엔티티 타입 클래스, 기본기 타입>
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+    List<Item> findByItemNm(String itemNm);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value = "select * from Item i where i.item_detail like CONCAT('%',:itemDetail,'%') order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+}
